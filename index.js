5c9f2876745cbd9346e90208833fed7eca47c793
Dasarad007 <ndasarad@gmail.com>
2025-09-28 12:19:22 +0530

    codee

--- index.js
+++ index.js
@@ -14,6 +14,34 @@ app.use(cors())
 
 app.use("/api/books",router)
 
+
+const Book = require("./path-to-your-model-file");
+
+
+Book.create({
+    title: "it starts with us",
+    author: "Paulo Coelho",
+    city: "dowleshwaram",
+    price: 3500,
+    available: true
+})
+
+Book.create({
+    title : "it ends with us",
+    author : "heisenberg",
+    city : "amalapuram",
+    price : 2000,
+    available : true
+})
+
+Book.create({
+    title : "love hypothesis",
+    author :"prassana",
+    city : "kovvur",
+    price : 40,
+    available : false
+})
+
 var PORT = process.env.PORT || 2026
 app.listen(PORT,()=>{
     console.log("server is running");
